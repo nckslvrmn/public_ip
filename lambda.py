99cb6c0ed4947e254e918e10a3b65842a82fb544
Nick Silverman <nckslvrmn@gmail.com>
2021-06-14 13:47:07 -0400

    some minor changes to be safer at getting data and error handle

--- lambda.py
+++ lambda.py
@@ -1,16 +1,20 @@
-import json
+def handler(event, _):
+    client_ip = event.get('headers', {}).get('x-forwarded-for').split(',')[0]
+    response_code = 200
 
-def handler(event, context):
-    client_ip = event['headers']['x-forwarded-for'].split(',')[0]
-    if event['queryStringParameters']['format'] == 'json':
-        body = json.dumps({ 'ip': client_ip })
+    if client_ip is None:
+        body = 'couldnt retrieve public IP from x-forwarded-for header'
+        content_type = 'text/plain'
+        response_code = 500
+    elif event.get('queryStringParameters', {}).get('format') == 'json':
+        body = f'{ "ip": "{client_ip}" }'
         content_type = 'application/json'
     else:
         body = client_ip
         content_type = 'text/plain'
 
     return {
-        'statusCode': 200,
+        'statusCode': response_code,
         'headers': {
             'Content-Type': content_type,
             'Cache-Control': 'no-cache, no-store, must-revalidate',
